90ff281b661b9854a8b29db39a3bbde3d50d7b37
ArthurSilvaChaves <161636185+ArthurSilvaChaves@users.noreply.github.com>
2026-05-07 11:23:41 -0300

    pagina 'calorias diarias' funcionando|ainda faltando alguns indices e melhor estilizacao (css)

--- js/kcaldia.js
+++ js/kcaldia.js
@@ -8,11 +8,29 @@ function addref(btn){
     const li = document.createElement("li");
 
     li.innerHTML = `
-        <input placeholder="nome do alimento">
+        <input placeholder="nome do alimento" type="text">
+        <input placeholder="quantidade de kcal" type="number" class="kcal">
+        <input placeholder="quantidade de gramas" type="number" class="g">
         <button onclick="this.parentElement.remove()">X</button>
     `;
 
     ul.appendChild(li)
+}
+
+function calckcal(botao) {
+    const refeicao = botao.parentElement;
+
+    const inputKcal = refeicao.querySelectorAll(".kcal");
+
+    let total = 0;
+
+    inputKcal.forEach(input => {
+        total += Number(input.value);
+    })
+
+    const p  = refeicao.querySelector("p");
+
+    p.textContent = `Total: ${total}kcal`
 }
 
 refeicoes.forEach(refeicao => {
@@ -22,11 +40,13 @@ refeicoes.forEach(refeicao => {
     div.innerHTML = `
         <h2>${refeicao}</h2>
         
-        <button onclick="addref(this)">+ ref</button>
+        <button onclick="addref(this)">+</button>
         <ul></ul>
         <!--funcao calckcal ainda nao foi feita-->
         <button onclick="calckcal(this)">Calcular</button>
+        <p>Total: </p>
     `;
+
 
     container.appendChild(div);
 });
