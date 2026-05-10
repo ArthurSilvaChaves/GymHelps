be524a946cadf2fd1d5a3b1e2d1f08b6bd3058aa
ArthurSilvaChaves <161636185+ArthurSilvaChaves@users.noreply.github.com>
2026-05-09 21:55:22 -0300

    localStorage funcionando na pagina de calorias diarias

--- js/kcaldia.js
+++ js/kcaldia.js
@@ -1,12 +1,51 @@
-
+window.onload = load;
 document.addEventListener("input",save)
 
 const refeicoes = ["Café da Manhã","Almoço","Jantar","Outra"];
 const container = document.getElementById("calculadora");
 
-const alimentos = [
-    ["arroz branco cozido",1.3]
-]
+// const alimentos = [
+//     ["arroz branco cozido",1.3]
+// ]
+
+
+function criarRefeicao(nomeRefeicao,alimentos =[]){
+    const div = document.createElement("div");
+    div.className = "refeicao";
+
+    div.innerHTML = `
+        <h2>${nomeRefeicao}</h2>
+        
+        <button onclick="addref(this)">+</button>
+
+        <ul></ul>
+
+        <button onclick="calckcal(this)">Calcular</button>
+
+        <p>Total: </p>
+    `;
+
+    const ul = div.querySelector("ul");
+    ul.type = "none"
+
+    alimentos.forEach(alimento => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+                <input value="${alimento.nome}" placeholder="nome" type="text"/>
+                <input value="${alimento.kcal}" placeholder="kcal" type="number" class="kcal"/>
+                <input value="${alimento.g}" placeholder="g" type="number" class="g"/>
+
+                <button onclick="this.parentElement.remove()">X</button>
+
+                <hr style="border: 1px black solid"/>
+        `;
+
+        ul.appendChild(li);
+    });
+
+    container.appendChild(div);
+}
 
 function addref(btn){
     const ul = btn.parentElement.querySelector("ul");
@@ -18,7 +57,7 @@ function addref(btn){
         <input placeholder="nome" type="text"/>
         <input placeholder="kcal" type="number" class="kcal"/>
         <input placeholder="g" type="number" class="g"/>
-        <button onclick="this.parentElement.remove()">X</button>
+        <button onclick="this.parentElement.remove(); save()">X</button>
 
         <hr style="border: 1px black solid"/>
     `;
@@ -52,17 +91,17 @@ function save(){
     const data = [];
      
     document.querySelectorAll(".refeicao").forEach(refeicao => {
-        const refeicaoNome = refeicao.querySelector("h2");
+        const refeicaoNome = refeicao.querySelector("h2").textContent;
     
-         const refeicoes = [];
+         const alimentos = [];
 
         refeicao.querySelectorAll("li").forEach(li => {
         const inputs = li.querySelectorAll("input");
 
-            refeicoes.push({
+            alimentos.push({
                 nome:inputs[0].value,
                 kcal:inputs[1].value,
-                g:inputs[2]
+                g:inputs[2].value
             });
         });
 
@@ -75,17 +114,23 @@ function save(){
     localStorage.setItem("dieta",JSON.stringify(data));
 }
 
-refeicoes.forEach(refeicao => {
-    const div = document.createElement("div");
-    div.className = "refeicao";
+function load(){
+    const data = JSON.parse(localStorage.getItem("dieta"));
 
-    div.innerHTML = `
-        <h2>${refeicao}</h2>
-        
-        <button onclick="addref(this)">+</button>
-        <ul></ul>
-        <button onclick="calckcal(this)">Calcular</button>
-        <p>Total: </p>
-    `;
-    container.appendChild(div);
-});
+    container.innerHTML = "";
+
+    if(!data){
+        refeicoes.forEach(refeicao => {
+            criarRefeicao(refeicao);
+        });
+
+        return;
+    }
+
+    data.forEach(refeicao => {
+        criarRefeicao(
+            refeicao.refeicaoNome,
+            refeicao.alimentos
+        );
+    });
+}
